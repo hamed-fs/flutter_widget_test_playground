4c33b91e21656b20c592529ea97308305da13c4b
hamed-fs <hamed@firstsource.io>
2020-07-26 15:22:29 +0430

    add sticky list

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_widget_test_playground/chart_setting.dart';
 import 'package:flutter_widget_test_playground/expandable_bottom_sheet.dart';
 import 'package:flutter_widget_test_playground/position_item.dart';
+import 'package:sticky_and_expandable_list/sticky_and_expandable_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -45,16 +46,106 @@ ExpandableBottomSheet _buildExpandableBottomSheet() => ExpandableBottomSheet(
               move against your favour.
             ''',
       upperContent: const ChartSetting(),
-      lowerContent: ListView.separated(
-        itemCount: 100,
-        itemBuilder: (BuildContext context, int index) => PositionItem(
-          contract: Contract(),
-          key: UniqueKey(),
-          onTap: (Contract contract) => print(contract.toString()),
-        ),
-        separatorBuilder: (BuildContext context, int item) => Container(
-          color: const Color(0xFF0E0E0E),
-          height: 1,
+      lowerContent: getListItems(),
+      // lowerContent: ListView.separated(
+      //   itemCount: 100,
+      //   itemBuilder: (BuildContext context, int index) => PositionItem(
+      //     contract: Contract(),
+      //     key: UniqueKey(),
+      //     onTap: (Contract contract) => print(contract.toString()),
+      //   ),
+      //   separatorBuilder: (BuildContext context, int item) => Container(
+      //     color: const Color(0xFF0E0E0E),
+      //     height: 1,
+      //   ),
+      // ),
+    );
+
+///
+Widget getListItems() {
+  final List<ContractExpand> sectionList = MockData.getExampleSections();
+  return ExpandableListView(
+    shrinkWrap: true,
+    builder: SliverExpandableChildDelegate<Contract, ContractExpand>(
+      sectionList: sectionList,
+      headerBuilder: (BuildContext context, int sectionIndex, int index) =>
+          Container(
+        height: 52,
+        width: double.infinity,
+        color: const Color(0xFF0E0E0E),
+        padding: const EdgeInsets.only(left: 16, top: 24),
+        child: Text(
+          sectionList[sectionIndex].header,
+          style: const TextStyle(
+            color: Color(0xFFC2C2C2),
+            fontSize: 14,
+          ),
         ),
       ),
-    );
+      itemBuilder: (
+        BuildContext context,
+        int sectionIndex,
+        int itemIndex,
+        int index,
+      ) =>
+          PositionItem(contract: sectionList[sectionIndex].items[itemIndex]),
+      separatorBuilder: (
+        BuildContext context,
+        bool isSectionSeparator,
+        int index,
+      ) =>
+          Container(color: const Color(0xFF0E0E0E), height: 1),
+    ),
+  );
+}
+
+///
+class ContractExpand implements ExpandableListSection<Contract> {
+  ///
+  ContractExpand(this.header, this.items);
+
+  ///
+  final List<Contract> items;
+
+  ///
+  final String header;
+
+  bool _expanded;
+
+  @override
+  List<Contract> getItems() => items;
+
+  @override
+  bool isSectionExpanded() => _expanded;
+
+  @override
+  void setSectionExpanded(bool expanded) {
+    _expanded = expanded;
+  }
+}
+
+///
+class MockData {
+  ///return a example list, by default, we have 10 sections,
+  ///each section has 5 items.
+  static List<ContractExpand> getExampleSections([
+    int sectionSize = 10,
+    int itemSize = 5,
+  ]) {
+    final List<ContractExpand> sections = <ContractExpand>[];
+
+    for (int i = 0; i < sectionSize; i++) {
+      final ContractExpand section = ContractExpand(
+        '0$i Jan 2020',
+        List<Contract>.generate(
+          itemSize,
+          (int index) => Contract(),
+        ),
+      ).._expanded = true;
+
+      sections.add(section);
+    }
+
+    return sections;
+  }
+}
